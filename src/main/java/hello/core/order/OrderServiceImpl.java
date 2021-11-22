ac9d75d421120f11a628e5bbcd050915cdc879ee
cheoljin408 <kcj409@naver.com>
2021-11-22 22:03:58 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용
    새로운 할인 정책 적용과 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,7 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +9,18 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    /*
+        위의 코드는 FixDiscountPolicy에서 RateDiscountPolicy로 구현체를 바꾸면 클라이언트인 OrderServiceImpl의 코드도 함께 변경해야 하므로
+        OCP 위반에 해당한다
+
+        또한 OrderServiceImpl은 DiscountPolicy 인터페이스 뿐만 아니라 구체클래스도 함께 의존하므로 DIP 위반에도 해당한다
+     */
+    // 인터페이스에만 의존하도록 설계를 변경해야 한다
+    private DiscountPolicy discountPolicy;
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
